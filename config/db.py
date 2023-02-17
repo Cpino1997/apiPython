c11395a59168db8aa81231bdf1531c6fa5530676
Cristian Pino <pino440@gmail.com>
2023-02-17 12:33:19 -0300

    🔥 test workflow 🔥

--- config/db.py
+++ config/db.py
@@ -1,8 +1,15 @@
+import os
 import pymysql
 
 
 def obtener_conexion():
-    return pymysql.connect(host='localhost',
-                           user='root',
-                           password='root',
-                           db='app')
+    return pymysql.connect(host=host,
+                           user=user,
+                           password=password,
+                           db=db)
+
+
+host = os.environ.get('DB_HOST', 'localhost')
+user = os.environ.get('DB_USER', 'root')
+password = os.environ.get('DB_PASSWORD', 'root')
+db = os.environ.get('DB_NAME', 'app')
