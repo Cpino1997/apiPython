ca6407d271aa8e92b5e1029e9fca520370e725bf
Cristian Pino <pino440@gmail.com>
2023-02-23 19:47:48 -0300

    🔥Integracion de Perfil🔥

--- src/pages/cuentas/[id].js
+++ src/pages/cuentas/[id].js
@@ -1,6 +1,8 @@
-import { useRouter } from 'next/router'
-import { useState, useEffect } from 'react'
+import {useRouter} from 'next/router'
+import {useEffect, useState} from 'react'
 import Navbar from "@/pages/componentes/navbar";
+import Link from "next/link";
+
 
 export default function Cuenta() {
     const router = useRouter()
@@ -9,38 +11,90 @@ export default function Cuenta() {
     const [isLoading, setIsLoading] = useState(true)
     const [isError, setIsError] = useState(false)
     const [update, setUpdate] = useState(false)
+    const [usuario, setUsuario] = useState('');
+    const [correo, setCorreo] = useState('');
+    const [roles, setRoles] = useState('');
+    const [password, setPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [isSuccess, setIsSuccess] = useState(false);
 
-    const handleUpdate = (e) => {
+    function handleUpdate(e){
         e.preventDefault()
         setUpdate(true)
     }
+    async function handleComprobar() {
+        try {
+            const idNumerico = parseInt(id, 10);
+            const token = localStorage.getItem('access_token');
+            const headers = {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            };
+            const response = await fetch(process.env.API_ENDPOINT + `/auth/password/${idNumerico}`,{
+                method: 'PUT',
+                headers: headers,
+                body: JSON.stringify({password:password, new_password:newPassword}),
+            });
+            console.log(response.mensaje)
+
+        }catch (e) {
+            setIsError(true)
+            setIsLoading(false)
+        }
+
+    }
+
 
     async function actualizarCuenta(cuentaActualizada, id) {
-        
+        try {
+            const idNumerico = parseInt(id, 10);
+            const token = localStorage.getItem("access_token");
+            const headers = {
+                'Authorization': `Bearer ${token}`,
+                "Content-Type": "application/json",
+            };
+            const res = await fetch(
+                `${process.env.API_ENDPOINT}/cuentas/${idNumerico}`,
+                {
+                    method: "PUT",
+                    headers: headers,
+                    body: JSON.stringify(cuentaActualizada),
+                }
+            );
+            const data = await res.json();
+            setCuenta(data);
+            setIsLoading(false);
+            setIsSuccess(true);
+            setTimeout(() => {
+                setIsSuccess(false);
+            }, 3000);
+        } catch (e) {
+            setIsError(true);
+            setIsLoading(false);
+        }
     }
 
     const handleSave = async () => {
-        setUpdate(false)
+        setUpdate(false);
         const cuentaActualizada = {
-            usuario: document.getElementById("usuario").value,
-            correo: document.getElementById("correo").value,
-            roles: document.getElementById("roles").value
-        }
-        await actualizarCuenta(cuentaActualizada, id)
-        getCuenta()
-    }
+            usuario: usuario, correo: correo, roles: roles
+        };
+        await actualizarCuenta(cuentaActualizada, id);
+        await getCuenta();
+    };
 
-    const getCuenta = async () => {
+
+    async function getCuenta() {
         try {
+            const token = localStorage.getItem('access_token');
+            const headers = {
+                'Authorization': `Bearer ${token}`
+            };
             const idNumerico = parseInt(id, 10);
             if (isNaN(idNumerico)) {
                 await router.push('/cuentas');
                 return;
             }
-            const token = localStorage.getItem('access_token');
-            const headers = {
-                'Authorization': `Bearer ${token}`
-            };
             const res = await fetch(process.env.API_ENDPOINT + `/cuentas/${idNumerico}`, {headers})
             const data = await res.json()
             setCuenta(data)
@@ -56,13 +110,27 @@ export default function Cuenta() {
     }, [])
 
     if (isLoading) {
-        return <div>Cargando...</div>
+        return (
+            <>
+                <div className={"text-center m-5"}>
+                    <div>Cargando...</div>
+                </div>
+            </>
+        )
     }
 
     if (isError) {
-        return <div>Error...</div>
+        return (
+            <>
+                <Navbar></Navbar>
+                <div className={"text-center m-5"}>
+                    <p>Error al obtener la cuenta! ...</p>
+                    <br />
+                    <Link href={"/cuentas"} className={"btn btn-lg btn-outline-dark"} type={"button"}>Volver</Link>
+                </div>
+            </>
+        )
     }
-
     return (
         <>
         <Navbar />
@@ -71,20 +139,30 @@ export default function Cuenta() {
                 <div className="card-body">
                     <h5 className="card-title">Cuenta</h5>
                     <div className="form-group row g-3">
+                        {isSuccess && (
+                            <div className="col-md-12">
+                                <div className="alert alert-success" role="alert">
+                                    <div className={"text-center"}>La cuenta se actualizó con éxito.</div>
+                                </div>
+                            </div>
+                        )}
+
                         <div className="col-md-6">
                             <label htmlFor="usuario" className="form-label">Usuario</label>
                             {update ? (
-                                <input type="text" className="form-control" id="usuario" defaultValue={cuenta.usuario} />
+                                <input type="text" className="form-control" name={"usuario"} id="usuario" defaultValue={cuenta.usuario}
+                                       onChange={(event) => setUsuario(event.target.value)}/>
                             ) : (
-                                <p>{cuenta.usuario}</p>
+                                <p onChange={(event) => setUsuario(event.target.value)}>{cuenta.usuario}</p>
                             )}
                         </div>
                         <div className="col-md-6">
                             <label htmlFor="roles" className="form-label">Roles</label>
                             {update ? (
-                                <input type="text" className="form-control" id="roles" defaultValue={cuenta.roles} />
+                                <input type="text" className="form-control" id="roles" defaultValue={cuenta.roles}
+                                       onChange={(event) => setRoles(event.target.value)}/>
                             ) : (
-                                <p>{cuenta.roles}</p>
+                                <p onChange={(event) => setRoles(event.target.value)}>{cuenta.roles}</p>
                             )}
                         </div>
                     </div>
@@ -92,9 +170,10 @@ export default function Cuenta() {
                         <div className="col-md-12">
                             <label htmlFor="correo" className="form-label">Correo</label>
                             {update ? (
-                                <input type="email" className="form-control" id="correo" defaultValue={cuenta.correo} />
+                                <input type="email" className="form-control" id="correo" defaultValue={cuenta.correo}
+                                       onChange={(event) => setCorreo(event.target.value)}/>
                             ) : (
-                                <p>{cuenta.correo}</p>
+                                <p onChange={(event) => setCorreo(event.target.value)}>{cuenta.correo}</p>
                             )}
                         </div>
                     </div>
@@ -120,15 +199,34 @@ export default function Cuenta() {
                 <div className="modal-dialog modal-dialog-centered">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">Cambio de password</h1>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
-                            ...
+                            <div className="form-group row g-3">
+                                <div className="col-md-12">
+                                    <label htmlFor="password" className="form-label">Contraseña</label>
+                                    <input type="password"
+                                           className="form-control"
+                                           id="password"
+                                           value={password}
+                                           onChange={(event) => setPassword(event.target.value)} />
+                                </div>
+                                <div className="form-group row g-3">
+                                    <div className="col-md-12">
+                                        <label htmlFor="newPassword" className="form-label">Nueva Contraseña</label>
+                                        <input type="password"
+                                               className="form-control"
+                                               id="newPassword"
+                                               value={newPassword}
+                                               onChange={(event) => setNewPassword(event.target.value)} />
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Save changes</button>
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                            <button type="button" className="btn btn-primary" onClick={handleComprobar}>Cambiar Contraseña</button>
                         </div>
                     </div>
                 </div>
--- src/pages/perfil/index.js
+++ src/pages/perfil/index.js
@@ -1,13 +1,11 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function Perfil() {
     const [cuenta, setCuenta] = useState({});
     const [isError, setIsError] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [password, setPassword] = useState("");
-    const [isUpdating, setIsUpdating] = useState(false);
 
     const router = useRouter();
 
@@ -40,46 +38,6 @@ export default function Perfil() {
         }
     };
 
-    const handleChangePasswordClick = () => {
-        setIsModalOpen(true);
-    };
-
-    const handleModalClose = () => {
-        setIsModalOpen(false);
-        setPassword("");
-    };
-
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
-    };
-
-    const handleSave = async () => {
-        setIsUpdating(true);
-        const access_token = localStorage.getItem("access_token");
-        const headers = {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${access_token}`,
-        };
-        const body = JSON.stringify({ ...cuenta, password });
-        try {
-            const response = await fetch(process.env.API_ENDPOINT + "/cuentas/update", {
-                method: "PUT",
-                headers,
-                body,
-            });
-            if (!response.ok) {
-                throw new Error("Error al actualizar cuenta");
-            }
-            const data = await response.json();
-            setCuenta(data);
-            setIsModalOpen(false);
-        } catch (error) {
-            setIsError(true);
-        } finally {
-            setIsUpdating(false);
-        }
-    };
-
     if (isLoading) {
         return (
             <div className={"container w-100"}>
@@ -89,7 +47,7 @@ export default function Perfil() {
     }
 
     if (isError) {
-        return <div>Error al cargar cuentas</div>;
+        return <div>Error al cargar el perfil</div>;
     }
 
     return (
@@ -104,39 +62,13 @@ export default function Perfil() {
                 <br />
                 <span>{cuenta.correo}</span>
             </div>
-            <button className={"btn btn-primary"} onClick={handleChangePasswordClick}>
-                Cambiar Contraseña
-            </button>
-            {isModalOpen && (
-                <div className={"modal"}>
-                    <div className={"modal-content"}>
-                        <div className={"modal-header"}>
-                            <h4>Cambiar Contraseña</h4>
-                            <button className={"btn-close"} onClick={handleModalClose} />
-                        </div>
-                        <div className={"modal-body"}>
-                            <label>Nueva contraseña: </label>
-                            <input type={"password"} value={password} onChange={handlePasswordChange} />
-                        </div>
-                        <div className={"modal-footer"}>
-                            {!isUpdating ? (
-                                <>
-                                    <button className={"btn btn-secondary"} onClick={handleModalClose}>
-                                        Cancelar
-                                    </button>
-                                    <button className={"btn btn-primary"} onClick={handleSave}>
-                                        Guardar
-                                    </button>
-                                </>
-                            ) : (
-                                <button className={"btn btn-primary"} disabled>
-                                    Actualizando...
-                                </button>
-                            )}
-                        </div>
-                    </div>
-                </div>
-            )}
+            <Link
+                className={"btn btn-primary"}
+                href="/cuentas/[id]"
+                as={`/cuentas/${cuenta.id}`}
+            >
+                Editar
+            </Link>
         </div>
     );
 }
